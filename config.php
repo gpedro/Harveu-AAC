e6d8f7722351bdfaaea30a4eb69ef19462e216ae
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2014-04-14 14:00:33 -0300

    Update

--- config.php
+++ config.php
@@ -1,19 +1,47 @@
 <?php
-#Settings required for the proper functioning of Harveu AAC#
-#Configuration database
+// Settings required for the proper functioning of Harveu AAC
+// Configuration database
 $config['Database'] = array (
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'tfs'
 );
-#Configuration server informations
+// Configuration server informations
 $config['Server'] = array (
 	'name' => 'Harveu AAC',
 	'slogan' => 'Now your server has a super AAC!',
 	'url' => 'http://localhost/Harveu-AAC/'
 );
-#Configuration website and engine data
+$config['Cities'] = array(
+	1 => 'Main City',
+	2 => 'Second City'
+);
+$config['Vocations'] = array(
+	0 => 'None',
+	1 => 'Sorcerer',
+	2 => 'Druid',
+	3 => 'Paladin',
+	4 => 'Knight',
+	5 => 'Master Sorcerer',
+	6 => 'Elder Druid',
+	7 => 'Royal Paladin',
+	8 => 'Elite Knight'
+);
+$config['Worlds'] = array(
+	0 => 'Main world',
+	//1 => 'Second world'
+);
+$config['Groups'] = array(
+	0 => 'Player',
+	1 => 'Tutor',
+	2 => 'Senior Tutor',
+	3 => 'Gamemaster',
+	4 => 'Community Manager',
+	5 => 'God'
+);
+
+// Configuration website and engine data
 $config['Engine'] = array(
 	'layout' 	=> 'default'
 );
